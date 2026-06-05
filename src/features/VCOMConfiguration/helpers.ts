541be2597fac9b2534fd7f9b44b51d52d4dbfadc
SlavaNordic <265652304+SlavaNordic@users.noreply.github.com>
2026-06-05 17:04:25 +0200

    Add license

--- src/features/VCOMConfiguration/helpers.ts
+++ src/features/VCOMConfiguration/helpers.ts
@@ -1,1 +1,7 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
 export const xor = (a: boolean, b: boolean): boolean => a !== b; // No XOR for booleans in TypeScript
